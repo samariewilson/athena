51b5c34c27746ae047b66c8e05deadd4a43cbe44
Samarie Wilson <atomic2@earthlink.net>
2018-01-27 14:01:36 -0500

    Initial Commit

---
+++ SheHacks Project/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SheHacks Project
+//
+//  Created by Samarie Wilson on 1/27/18.
+//  Copyright © 2018 Samarie Wilson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SheHacks Project/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SheHacks Project
+//
+//  Created by Samarie Wilson on 1/27/18.
+//  Copyright © 2018 Samarie Wilson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
