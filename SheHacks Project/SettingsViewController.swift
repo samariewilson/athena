4da1db7523c11257d1b1e1c2678869ce0a815d4a
Somya Arora <arorasomya64@gmail.com>
2018-01-28 01:12:12 -0500

    the alert box to confirm the contact was added

--- SheHacks Project/SettingsViewController.swift
+++ SheHacks Project/SettingsViewController.swift
@@ -26,19 +26,27 @@ class SettingsViewController: UIViewController, MFMessageComposeViewControllerDe
 
         // Do any additional setup after loading the view.
     }
-
+//    override func viewDidAppear(_ animated: Bool) {
+//        createAlert(title: "Successfully added!", message: "")
+//    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     @IBAction func addInfo(_ sender: Any) {
-        let composeVC = MFMessageComposeViewController()
-        composeVC.body = txtName.text!
-        composeVC.recipients = [txtNumber.text!]
-        composeVC.messageComposeDelegate = self
-        
-        self.present(composeVC, animated: true, completion: nil )
+        let alert = UIAlertController(title: "", message: "Successfully Added!", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (action) in alert.dismiss(animated: true, completion: nil)
+            }))
+    
+        self.present(alert, animated: true, completion: nil)
+        // the bottom code is the messaging code
+//        let composeVC = MFMessageComposeViewController()
+//        composeVC.body = "I am in danger and my location is " + txtAddress.text!
+//        composeVC.recipients = [txtNumber.text!]
+//        composeVC.messageComposeDelegate = self
+//
+//        self.present(composeVC, animated: true, completion: nil )
     }
     func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
         
